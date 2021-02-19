c77e986d2fa140055e23b84e86e185800523f313
Lucas Pereira <lucaspereiradesouzat@gmail.com>
2021-02-19 12:36:04 -0300

    test config

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -5,10 +5,18 @@
 import 'react-native';
 import React from 'react';
 import App from '../src/App';
+import People from '../src/pages/People';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+let findTextElement = function (tree, element) {
+  console.warn(tree);
+  return true;
+};
+
+it('Find text element', () => {
+  let tree = renderer.create(<People />).toJSON();
+
+  expect(findTextElement(tree, 'Peso')).toBeDefined();
 });
--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFiles: [
+    '<rootDir>/jest/setup.js',
+    './node_modules/react-native-gesture-handler/jestSetup.js',
+  ],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@?react-navigation)',
+  ],
 };
